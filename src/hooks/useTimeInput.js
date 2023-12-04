bb0ffcadd9a58a4c5cbfe7eb8cf518180ff73289
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-12-04 07:15:36 -0500

    making time input stricter

--- src/hooks/useTimeInput.js
+++ src/hooks/useTimeInput.js
@@ -6,14 +6,14 @@ const useTimeInput = (initialMinutes = '00', initialSeconds = '00') => {
     const [calculatedTime, setCalculatedTime] = useState(parseInt(initialMinutes, 10) * 60 + parseInt(initialSeconds, 10));
 
     const handleMinutesChange = (e) => {
-        const newMinutes = e.target.value.slice(0, 2);
+        const newMinutes = e.target.value.replace(/[^0-9]/g, '').slice(0, 2);
         setInputMinutes(newMinutes);
         const newCalculatedTime = parseInt(newMinutes, 10) * 60 + parseInt(inputSeconds, 10);
         setCalculatedTime(newCalculatedTime);
     };
 
     const handleSecondsChange = (e) => {
-        const newSeconds = e.target.value.slice(0, 2);
+        const newSeconds = e.target.value.replace(/[^0-9]/g, '').slice(0, 2);
         setInputSeconds(newSeconds);
         const newCalculatedTime = parseInt(inputMinutes, 10) * 60 + parseInt(newSeconds, 10);
         setCalculatedTime(newCalculatedTime);
